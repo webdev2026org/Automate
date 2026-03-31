b6a9bf77d2e1c557f45591a43d2c5f9b273bb637
SUPRAKASH BALLAV <Suprakashballav1997@gmail.com>
2026-03-31 23:12:51 +0530

    Footer section added

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import Navbar from './layouts/global/Navbar'
+import Footer from './layouts/global/Footer'
+import ProductListViewScreen from './layouts/productLists/ProductListViewScreen'
 
 
 function App() {
@@ -7,6 +9,8 @@ function App() {
   return (
     <>
    <Navbar />
+   <ProductListViewScreen/>
+   <Footer/>
     </>
   )
 }
