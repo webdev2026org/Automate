5cf169e6d0db5241db84c53fa1c0f48b0ad5f8ba
Rakesh karmakar <rakeshkarmakar23091998@gmail.com>
2026-04-01 11:47:56 +0530

    feature/updated-global-navbar-conditions

--- src/layouts/global/Navbar.jsx
+++ src/layouts/global/Navbar.jsx
@@ -1,35 +1,43 @@
 import { FaStore, FaSearch, FaUser, FaShoppingCart } from "react-icons/fa";
+import Breadcrumb from "./BreadCrum";
 
-const Navbar = () => {
+const Navbar = (props) => {
     return (
         <nav className='Navbar'>
 
             <div className="Left-Navbar-Section">
                 {/* Logo */}
                 <div className='Navbar-logo'>
-                    <FaStore />
+                    {props.pageIcon === 'store' && <FaStore />}
+                    {props.pageIcon === 'cart' && <FaShoppingCart />}
                     <span className="Navbar-header">AccessCommerce</span>
                 </div>
                 {/* Navigation Links */}
-                <ul className='Navbar-links'>
+                {props.showmenuOtions && (<ul
+                 className='Navbar-links'>
                     <li><a href='/' className='Navbar-link'>Home</a></li>
                     <li><a href='/' className='Navbar-link'>Shop</a></li>
                     <li><a href='/' className='Navbar-link'>Product detail</a></li>
                     <li><a href='/' className='Navbar-link'>Cart</a></li>
-                </ul>
+                </ul>)}
+                {props.breadcrumbItems.length!==0 && props.breadcrumbItems && (
+                    <div className='Navbar-breadcrumb'>
+                        <Breadcrumb items={props.breadcrumbItems} />
+                    </div>
+                )}
 
             </div>
 
             <div className="Right-Navbar-Section">
                 {/* Search + Icons */}
                 <div className='flex items-center'>
-                    <div className='Navbar-Search'>
+                    {props.showSearchBar && (<div className='Navbar-Search'>
                         <FaSearch className="Navbar-Search-icon" />
                         <input type='text' placeholder='Search ..'></input>
-                    </div>
+                    </div>)}
                     <div className='Navbar-icons'>
-                        <FaUser className="Navbar-icon" />
-                        <FaShoppingCart className="Navbar-icon" />
+                        {props.userIconType==='user-icon' && <FaUser className="Navbar-icon" />}
+                        {props.showCartIcon && <FaShoppingCart className="Navbar-icon" />}
                     </div>
                 </div>
             </div>
--- src/layouts/productLists/ProductListViewScreen.jsx
+++ src/layouts/productLists/ProductListViewScreen.jsx
@@ -3,9 +3,17 @@ import Footer from '../global/Footer'
 
 
 const ProductListViewScreen = () => {
+
   return (
       <div className='app-container flex flex-col min-h-screen justify-between'>
-   <Navbar />
+   <Navbar 
+   pageIcon='store'
+   showSearchBar={true}
+   showmenuOtions={false}
+   breadcrumbItems={[]}
+   showCartIcon={true}
+   userIconType='user-icon'
+   />
     <main className='container mx-auto px-4 py-8'> main container of List view</main>
    <Footer/>
     </div>
