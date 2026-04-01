778e7efe40f60e55aac81867d3fb4d61d9b416fb
Rakesh karmakar <rakeshkarmakar23091998@gmail.com>
2026-04-01 13:21:41 +0530

    updated Login UI

--- src/layouts/loginSignUp/Login.jsx
+++ src/layouts/loginSignUp/Login.jsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import Navbar from "../global/Navbar"
+import Footer from "../global/Footer"
+
 
 function Login() {
   return (
-    <div>Login</div>
+    <div className='app-container flex flex-col min-h-screen justify-between'>
+   <Navbar 
+   showSearchBar={false}
+   showmenuOtions={false}
+   breadcrumbItems={[]}
+   showCartIcon={false}
+   showLoginBtn={true}
+   />
+    <main className='container mx-auto px-4 py-8'> Login Page</main>
+   <Footer/>
+    </div>
   )
 }
 
