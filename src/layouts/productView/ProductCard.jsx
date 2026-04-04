1ac3fea8b684ea7cd79cb85180e93cdb6dce811e
Rakesh karmakar <rakeshkarmakar23091998@gmail.com>
2026-04-04 20:59:47 +0530

    Create ProductCard.jsx

---
+++ src/layouts/productView/ProductCard.jsx
@@ -0,0 +1,40 @@
+import img2 from "../../assets/img2.png"
+import "../../styles/productcard.css"
+
+const ProductCard = () => {
+    return (
+        <div className="product-card">
+            {/* Product Image */}
+            <div className='image-container'>
+                <img
+                    src={img2}
+                    alt="Stacked beverage crates"
+                />
+            </div>
+
+
+            {/* Product Info */}
+            <div className="card-description">
+
+               <div className='card-section'>
+                <span className="font-medium">Price:</span> 
+                <span>$49.99</span>
+                <button className="card-btn">
+                    Add
+                </button>
+               </div>
+                
+               <div className='card-section'>
+                <span className="font-medium">Availability:</span> 
+                <span>In stock</span>
+                <button className="card-btn">
+                    Details
+                </button>
+               </div>
+
+            </div>
+        </div>
+    )
+}
+
+export default ProductCard
