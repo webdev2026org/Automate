2aa6d2de884e9f9a5fa23f898b0db2ec2b14a51d
SUPRAKASH BALLAV <Suprakashballav1997@gmail.com>
2026-04-04 15:56:50 +0530

    Update ProductCard.jsx

--- src/layouts/productView/ProductCard.jsx
+++ src/layouts/productView/ProductCard.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import img2 from "../../assets/img2.png"
+import "../../styles/productCard.css"
+
 const ProductCard = () => {
     return (
-        <div className="max-w-sm bg-white rounded-lg shadow-md overflow-hidden">
+        <div className="Product-card">
             {/* Product Image */}
-            <div className='ImageContainer bg-gray-300'>
+            <div className='ImageContainer'>
                 <img
                     src={img2}
                     alt="Stacked beverage crates"
@@ -14,20 +16,20 @@ const ProductCard = () => {
 
 
             {/* Product Info */}
-            <div className="Card-Description flex justify-between bg-black text-white ">
+            <div className="Card-Description">
 
-               <div className='Card-Left-Section flex flex-col w-full m-6 gap-2'>
+               <div className='Card-Section'>
                 <span className="font-medium">Price:</span> 
                 <span>$49.99</span>
-                <button className="flex-1 bg-white text-black py-3 px-3 hover:bg-gray-800">
+                <button className="Card-btn">
                     Add
                 </button>
                </div>
                 
-               <div className='Card-Right-Section flex flex-col w-full m-6 gap-2'>
+               <div className='Card-Section'>
                 <span className="font-medium">Availability:</span> 
                 <span>In stock</span>
-                <button className="flex-1 bg-white text-black px-3 py-3 hover:bg-gray-800">
+                <button className="Card-btn">
                     Details
                 </button>
                </div>
